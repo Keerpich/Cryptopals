bddd1004d082d9d0769d18e116add3dd63aa8905
Daniel Alexandru Radu <danielalexandru.radu@gmail.com>
2016-03-25 11:06:08 +0200

    set1 challenge 6 in progress

---
+++ Cryptopals/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cryptopals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2
+            //Console.WriteLine(Set1.FixedXOR("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"));
+
+            //3
+            //Console.WriteLine(Set1.SingleByteXORCipher("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").clearText);
+
+            //4
+            //Console.WriteLine(Set1.DetectSingleCharacterXOR(File.ReadAllLines("input.txt")).clearText);
+
+            //5
+            //Console.WriteLine(Set1.RepeatingKeyXOR("Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal", "ICE"));
+
+            //6
+            //Console.WriteLine(Utils.HammingDistance("this is a test", "wokka wokka!!!"));
+            
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Cryptopals/Set1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cryptopals
+{
+    class Set1
+    {
+
+        #region BaseConversion
+        public static string HexToB64(string hex)
+        {
+            return Convert.ToBase64String(HexStringToByteArray(hex));
+        }
+
+        public static string B64ToString(string b64)
+        {
+            return HexByteArrayToString(Convert.FromBase64String(b64));
+        }
+
+        public static byte[] HexStringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static string HexByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        public static string ByteArrayToASCIIString(byte[] ba)
+        {
+            return Encoding.ASCII.GetString(ba);
+        }
+
+        #endregion
+
+        #region XOROperations
+        public static string FixedXOR(string hex1, string hex2)
+        {
+            byte[] op1 = HexStringToByteArray(hex1), op2 = HexStringToByteArray(hex2);
+            byte[] result = new byte[op1.Length];
+
+            for (int i = 0; i < op1.Length; i++)
+            {
+                result[i] = (byte)(op1[i] ^ op2[i]);
+
+            }
+
+            return HexByteArrayToString(result);
+        }
+
+        public static string SingleByteFullHex(string cipher, byte key)
+        {
+            byte[] hexByte = HexStringToByteArray(cipher);
+
+            byte[] result = new byte[hexByte.Length];
+
+            for(int i = 0; i < hexByte.Length; i++)
+            {
+                result[i] = (byte)(hexByte[i] ^ key);
+            }
+
+            return HexByteArrayToString(result);
+        }
+
+        public static string RepeatingKeyXOR(string cipher, string key)
+        {
+            byte[] cipherBytes = ASCIIEncoding.ASCII.GetBytes(cipher);
+            byte[] keyBytes = ASCIIEncoding.ASCII.GetBytes(key);
+
+            byte[] result = new byte[cipherBytes.Length];
+
+            for (int i = 0; i < cipherBytes.Length; i++)
+            {
+                result[i] = (byte)(cipherBytes[i] ^ keyBytes[i % keyBytes.Length]);
+            }
+
+            return HexByteArrayToString(result);
+        }
+        #endregion
+
+        #region Structs
+        public struct SingleByteDecryptResult
+        {
+            public int key;
+            public int fitness;
+            public string clearText;
+        }
+        #endregion
+
+        #region Ciphers
+        public static SingleByteDecryptResult SingleByteXORCipher(string hex)
+        {
+            List<SingleByteDecryptResult> results = new List<SingleByteDecryptResult>();
+
+            for (int k = 0; k < 256; k++)
+            {
+                string clearText = ByteArrayToASCIIString(HexStringToByteArray(SingleByteFullHex(hex, Convert.ToByte(k))));
+                int value = Utils.TotalFitnessOfString(clearText);
+
+                SingleByteDecryptResult sbdr = new SingleByteDecryptResult();
+                sbdr.key = k;
+                sbdr.fitness = value;
+                sbdr.clearText = clearText;
+
+                results.Add(sbdr);
+            }
+
+            results.Sort((r1, r2) => r1.fitness.CompareTo(r2.fitness));
+
+            return results[results.Count - 1];
+        }
+
+        public static SingleByteDecryptResult DetectSingleCharacterXOR(string[] allHexs)
+        {
+            List<SingleByteDecryptResult> bestResults = new List<SingleByteDecryptResult>();
+
+            foreach(string hex in allHexs)
+            {
+                bestResults.Add(SingleByteXORCipher(hex));
+            }
+
+            bestResults.Sort((r1, r2) => r1.fitness.CompareTo(r2.fitness));
+
+            return bestResults[bestResults.Count - 1];
+        }
+
+        public static string Vigenere(string base4CipheredText)
+        {
+            string cipheredText = B64ToString(base4CipheredText);
+            byte[] cipheredBytes = Utils.GetBytes(cipheredText);
+
+            int bestKeysize = -1;
+            float minDistance = -1f;
+
+            for (int keysize = 2; keysize < (40 < cipheredText.Length / 2 ? 40 : cipheredText.Length / 2); keysize++)
+            {
+                byte[] chunk1 = new byte[keysize];
+                byte[] chunk2 = new byte[keysize];
+
+                for (int i = 0; i < keysize; i++)
+                    chunk1[i] = cipheredBytes[i];
+                for (int i = keysize; i < 2 * keysize; i++)
+                    chunk2[i - keysize] = cipheredBytes[i];
+
+                float distance = Utils.HammingDistance(new BitArray(chunk1), new BitArray(chunk2)) / keysize;
+
+                if(distance < minDistance || minDistance == -1)
+                {
+                    minDistance = distance;
+                    bestKeysize = keysize;
+                }
+            }
+
+            //now we probably know the keysize
+            //and we break the text into chunk of bestKeysize length
+
+            List<byte[]> allChunks = new List<byte[]>();
+
+            for (int i = 0; i < cipheredBytes.Length; i+=bestKeysize)
+            {
+                int chunkLength = cipheredBytes.Length - i < bestKeysize ? cipheredBytes.Length - 1 : bestKeysize;
+                allChunks.Add(new byte[chunkLength]);
+
+                for (int j = 0; j < chunkLength; j++)
+                {
+                    allChunks[i][j] = cipheredBytes[i + j];
+                }
+            }
+
+            //make new blocks according to step 6
+
+            List<byte[]> allBlocks = new List<byte[]>();
+
+            throw new NotImplementedException();
+
+        }
+        #endregion
+
+    }
+}
